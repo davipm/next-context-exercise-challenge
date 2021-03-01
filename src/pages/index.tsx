8a52cab045eae43ca217e420972fb4fb54418109
davi <davi.p.m94@gmail.com>
2021-02-28 21:00:55 -0300

    init app

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import styles from "../styles/pages/Home.module.scss";
+
+import Profile from "../components/profile";
+import ChallengesProvider from "../contexts/challenges";
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home({
+  challengesCompleted,
+  currentExperience,
+  level,
+}: HomeProps) {
+  return (
+    <ChallengesProvider
+      level={level}
+      challengesCompleted={challengesCompleted}
+      currentExperience={currentExperience}
+    >
+      <div>
+        <main className={styles.container}>
+          <Head>
+            <title>Exercise App</title>
+          </Head>
+
+          <div />
+
+          <section>
+            <div>
+              <Profile />
+            </div>
+          </section>
+        </main>
+      </div>
+    </ChallengesProvider>
+  );
+}
