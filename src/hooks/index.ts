beb05827dd947fbb5723e0526c74f769e8e4aa46
davi <davi.p.m94@gmail.com>
2021-03-01 22:28:41 -0300

    creating countdown context

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 import { ChallengesContext } from "@/contexts/challenges";
+import { CountdownContext } from "@/contexts/countdown";
 
 export function useChallenges() {
   return useContext(ChallengesContext);
+}
+
+export function useCountdown() {
+  return useContext(CountdownContext);
 }
